13fb8f4116f3bb6a8c23bfe985483058964b12b0
root <root@z3db0y.com>
2020-12-23 15:41:49 +0000

    Auto committer.

--- rainbowRoleAPI.js
+++ rainbowRoleAPI.js
@@ -15,7 +15,7 @@ class RainbowRole {
             let guildSettings = getGuild(guildID);
             if(guildSettings.rainbowRoles.length() > 0) {
                 for(int i; i<guildSettings.rainbowRoles; i++) {
-                    var frequency = .3;
+                    var frequency = 0.3;
                     for (var i = 0; i < 32; ++i) {
                         red   = Math.sin(frequency*i + 0) * 127 + 128;
                         green = Math.sin(frequency*i + 2) * 127 + 128;
