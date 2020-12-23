17cb727b3247c044aa47d9f969239814197716cd
root <root@z3db0y.com>
2020-12-23 15:21:58 +0000

    Auto committer.

--- rainbowRoleAPI.js
+++ rainbowRoleAPI.js
@@ -1,10 +1,18 @@
 // RainbowRole API
 // 16:20 UTC+2 23 December 2020
 
+'use strict';
+const fs = require('fs');
+
 class RainbowRole {
+    getGuild(guildID) {
+        let guildFileName = guildID + ".json";
+        return JSON.parse(fs.readFileSync(guildFileName));
+    }
+
     async runRainbowRole(discordClient, guildID) {
         setIntrerval(10, function () {
-            
+            getGuild()
         });
     }
 }
