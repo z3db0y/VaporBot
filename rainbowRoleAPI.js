b0366149be8d8aff522da7eaa03bc938ff336b81
root <root@z3db0y.com>
2020-12-23 15:47:39 +0000

    Auto committer.

--- rainbowRoleAPI.js
+++ rainbowRoleAPI.js
@@ -26,7 +26,7 @@ class RainbowRole {
         });
     }
 
-    var rgbToHex = function (rgb) { 
+    var rgbToHex = function (rgb) {
         var hex = Number(rgb).toString(16);
         if (hex.length < 2) {
              hex = "0" + hex;
@@ -34,7 +34,7 @@ class RainbowRole {
         return hex;
     };
 
-    var fullColorHex = function(r,g,b) {   
+    var fullColorHex = function(r,g,b) {
         var red = rgbToHex(r);
         var green = rgbToHex(g);
         var blue = rgbToHex(b);
