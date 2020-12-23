6ebea96586a8354915e976341d605237cfa3ef19
root <root@z3db0y.com>
2020-12-23 14:39:11 +0000

    Auto committer.

---
+++ rainbowRoleAPI.js
@@ -0,0 +1,10 @@
+// RainbowRole API
+// 16:20 UTC+2 23 December 2020
+
+class RainbowRole {
+    async runRainbowRole(discordClient, guildID) {
+        setIntrerval(10, function () {
+            
+        });
+    }
+}
