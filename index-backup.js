a6983337c7f546a97c56d1426bebbed3f250c5e1
root <root@z3db0y.com>
2020-12-24 11:45:42 +0000

    Auto committer.

--- index-backup.js
+++ index-backup.js
@@ -183,7 +183,7 @@ client.on('message', (msg) => {
         msg.channel.send('Invalid usage! Use: ' + prefix + 'rainbowrole <ROLE>');
         return;
       }
-      msg.channel.send(`[Debug] I see ${msg.content.substring(prefix.length+12, prefix.length+31)}`);
+      concole.log(`\x1b[35m[Debug] \x1b[33mI saw ${msg.content.substring(prefix.length+12, prefix.length+31)}!`);
       let guildData = JSON.parse(fs.readFileSync(filename));
       msg.guild.roles.cache.find(role => role.id === msg.content.substring(prefix.length+12, prefix.length+31)).setColor(msg.guild.roles.find(role => role.id === msg.content.substring(prefix.length+12, prefix.length+31)).color)
       .then( () => {
