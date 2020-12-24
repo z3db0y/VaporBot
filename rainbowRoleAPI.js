018ace8d42e514d38d209601cdb4e884d2bcc4ae
root <root@z3db0y.com>
2020-12-24 16:15:44 +0000

    Auto committer.

--- rainbowRoleAPI.js
+++ rainbowRoleAPI.js
@@ -12,21 +12,21 @@ class RainbowRole {
 
     async runRainbowRole(discordClient, guildID) {
         setInterval(() => {
+            const guildSettings;
             try {
-                let guildSettings = getGuild(guildID);
+                guildSettings = getGuild(guildID);
                 console.log(`\x1b[35m[RainbowRole] \x1b[0mInitiated rainbow role API for \x1b[32m${discordClient.guilds.find(guild => guild.id === guildID).name}\x1b[0m!`);
-            } catch (err) {
-            }
-            if(guildSettings.rainbowRoles.length() > 0) {
-                for(var i = 0; i < 32; ++i) {
-                    var frequency = 0.3;
-                    for (var i1 = 0; i1 < guildSettings.rainbowRoles.length(); i++) {
-                        red   = Math.sin(frequency*i + 0) * 127 + 128;
-                        green = Math.sin(frequency*i + 2) * 127 + 128;
-                        blue  = Math.sin(frequency*i + 4) * 127 + 128;
+                if(guildSettings.rainbowRoles.length() > 0) {
+                    for(var i = 0; i < 32; ++i) {
+                        var frequency = 0.3;
+                        for (var i1 = 0; i1 < guildSettings.rainbowRoles.length(); i++) {
+                            red   = Math.sin(frequency*i + 0) * 127 + 128;
+                            green = Math.sin(frequency*i + 2) * 127 + 128;
+                            blue  = Math.sin(frequency*i + 4) * 127 + 128;
 
-                        var color = '0x'+fullColorHex(red,green,blue);
-                        discordClient.guilds.cache.find(guild => guild.id === guildID).roles.cache.find(role => role.id === guildSettings.rainbowRoles[i]).setColor(color);
+                            var color = '0x'+fullColorHex(red,green,blue);
+                            discordClient.guilds.cache.find(guild => guild.id === guildID).roles.cache.find(role => role.id === guildSettings.rainbowRoles[i]).setColor(color);
+                        }
                     }
                 }
             }
