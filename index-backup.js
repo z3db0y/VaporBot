9e137eb780f746b3e91efecf6f487e8bdc86aaeb
root <root@z3db0y.com>
2020-12-25 13:57:01 +0000

    Auto committer.

--- index-backup.js
+++ index-backup.js
@@ -228,7 +228,11 @@ client.on('message', (msg) => {
               fs.writeFileSync(process.env.CONFIG_PATH, JSON.stringify(botSettings,null,2));
               msg.channel.send('Added user as bot developer!');
             } else if(/^<@!/.test(args[1])) {
-              msg.channel.send('[Debug] Regex passed.');
+              botDevelopers.push(args[1].substring(3,args[1].length-1);
+              let botSettings = JSON.parse(fs.readFileSync(process.env.CONFIG_PATH));
+              botSettings.botDevelopers = botDevelopers;
+              fs.writeFileSync(process.env.CONFIG_PATH, JSON.stringify(botSettings,null,2));
+              msg.channel.send('Added user as bot developer!');
             } else {
               msg.channel.send('[Debug] Regex fail.');
             }
