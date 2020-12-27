5e4fc7f0ef9289e8f5da144a303f05cd91b24eb6
root <root@z3db0y.com>
2020-12-27 14:06:27 +0000

    Auto committer.

--- index-backup.js
+++ index-backup.js
@@ -21,13 +21,13 @@ client.on('ready', () => {
     if(BOT_CHANNEL == 0) {
       client.user.setPresence({activity: {type: "PLAYING", name: "Vapor Beta | Buggy and mostly offline"}, status: 'idle', afk: false})
       .then(() => {
-        console.log('\x1b[35m[Discord] \x1b[0mSet custom status (\x1b[32mSTABLE\x1b[0m)!');
+        console.log('\x1b[35m[Discord] \x1b[0mSet custom status (\x1b[32mBETA\x1b[0m)!');
       });
     }
     else if (BOT_CHANNEL == 1) {
       client.user.setPresence({activity: {type: "PLAYING", name: "Vapor | v!help"}, status: "online", afk: false})
       .then(() => {
-        console.log('\x1b[35m[Discord] \x1b[0mSet custom status (\x1b[32mBETA\x1b[0m)!');
+        console.log('\x1b[35m[Discord] \x1b[0mSet custom status (\x1b[32mSTABLE\x1b[0m)!');
       });
     }
     client.guilds.cache.forEach((guild) => {
