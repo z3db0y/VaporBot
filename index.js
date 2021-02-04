adc80a198683e9f972c51fa3ef8e8b02af83e54a
root <root@z3db0y.com>
2021-02-04 13:34:43 +0000

    Auto committer.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 let botChannels = { "BETA":0, "STABLE":1 };
 
-const BOT_CHANNEL = botChannels.STABLE;
+const BOT_CHANNEL = botChannels.BETA;
 
 require('dotenv').config();
 const Discord = require('discord.js');
@@ -180,9 +180,9 @@ client.on('message', (msg) => {
         if(userId.startsWith('!')) userId = userId.substring(1);
         let isBanned;
         msg.guild.fetchBans().then(bans => {
-          if(bans.get(userId)) {
-            isBanned=true
-          } else isBanned=false
+          if(bans.get(userId) == null) {
+            isBanned=false
+          } else isBanned=true
         });
         if(isBanned) {
           if(reason) {
@@ -196,9 +196,9 @@ client.on('message', (msg) => {
       } else if(/^[0-9]*$/.test(user)) {
         let isBanned;
         msg.guild.fetchBans().then(bans => {
-          if(bans.get(user)) {
-            isBanned=true
-          } else isBanned=false
+          if(bans.get(user) == null) {
+            isBanned=false
+          } else isBanned=true
         });
         if(isBanned) {
           if(reason) {
