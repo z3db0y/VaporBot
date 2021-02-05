637b2a6788bd07d26dbebc14ced532af37a6bf27
root <root@z3db0y.com>
2021-02-05 09:35:56 +0000

    Auto committer.

---
+++ updateAPI.js
@@ -0,0 +1,21 @@
+'use strict';
+const fs = require('fs');
+const { emit } = require('process');
+
+
+module.exports = {
+    currentVersion: null,
+    setUpdateChannel: function (channelID, guildID) {
+        let guildSettings = JSON.parse(fs.readFileSync(`${guildID}.json`));
+        guildSettings.updateChannel = channelID;
+        fs.writeFileSync(`${guildID}.json`, JSON.stringify(guildSettings, null, 2));
+    },
+    init: function (client) {
+        client.guilds.cache.forEach((guild) => {
+            setInterval(this.checkForUpdates(guild.id) ,60000);
+        });
+    },
+    checkForUpdates: function (guildID) {
+
+    }
+}
