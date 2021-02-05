10810c8ca499587bcf9871f676770dc0a2a52fc5
root <root@z3db0y.com>
2021-02-05 08:16:50 +0000

    Auto committer.

--- index-backup.js
+++ index-backup.js
@@ -380,7 +380,7 @@ client.on('message', (msg) => {
               }
           }
           guildData.rainbowRoles.push(msg.content.substring(prefix.length+15, prefix.length+33));
-          msg.channel.send('d('Enabled rainbow role for ' + msg.content.substring(prefix.length+12, prefix.length+33) + '>!');
+          msg.channel.send('Enabled rainbow role for ' + msg.content.substring(prefix.length+12, prefix.length+33) + '>!');
           fs.writeFileSync(filename, JSON.stringify(guildData, null, 2));
       })
       .catch((err) => {
