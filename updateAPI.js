6c9ee1eb88768bdb06e6e084e029ca965663181d
root <root@z3db0y.com>
2021-02-05 10:35:46 +0000

    Auto committer.

--- updateAPI.js
+++ updateAPI.js
@@ -8,14 +8,12 @@ function checkUpdate(guildID, client) {
     let releases = [];
     fs.readdir('./releases/', (err, files) => {
         files.forEach(file => {
-            if(fs.statSync(file).isFile()) {
-                console.log(file);
-                console.log(file.replace('.', '').replace('txt', ''));
-                if(/^[0-9]*$/.test(file.replace('.', '').replace('txt', ''))) releases[releases.length] = {
-                    filename: file,
-                    fileInt: parseInt(file.replace('.', '').replace('txt'))
-                };
-            }
+            console.log(file);
+            console.log(file.replace('.', '').replace('txt', ''));
+            if(/^[0-9]*$/.test(file.replace('.', '').replace('txt', ''))) releases[releases.length] = {
+                filename: file,
+                fileInt: parseInt(file.replace('.', '').replace('txt'))
+            };
         });
     });
     var largestFileInt = 0;
