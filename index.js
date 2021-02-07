a82ac489368aa42cf969f3a870d9a91a58532038
root <root@z3db0y.com>
2021-02-07 13:51:55 +0000

    Auto committer.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const updateAPI = require('./updateAPI');
 let botChannels = { "BETA":0, "STABLE":1 };
 
-const BOT_CHANNEL = botChannels.STABLE;
+const BOT_CHANNEL = botChannels.BETA;
 
 require('dotenv').config();
 const Discord = require('discord.js');
@@ -154,7 +154,7 @@ client.on('message', (msg) => {
         msg.channel.send({embed: {
           title: "Still Need Help?",
             color: `0x${msg.guild.me.displayHexColor.substring(1)}`,
-            description: "Join our support server! (\[link\])[https://discord.gg/ptg4EC9eyA]",
+            description: "Join our support server! [\[link\]](https://discord.gg/ptg4EC9eyA)",
             author: {
                 name: msg.author.tag,
                 icon_url: msg.author.avatarURL()
