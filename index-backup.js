2955f1e9b393f4e2b03f108e8b118316a4233d78
root <root@z3db0y.com>
2021-02-08 11:08:07 +0000

    Auto committer.

--- index-backup.js
+++ index-backup.js
@@ -563,9 +563,13 @@ client.on('message', (msg) => {
 
       client.guilds.cache.forEach(g => {
         botDevelopers.forEach(dev => {
-          console.dir(`\x1b[31m[DEBUG] \x1b[0m${g.members.fetch(dev)}`);
-          if(g.members.fetch(dev)) {
+          let isDevGuild = true;
+          g.members.fetch(dev) .catch(err => {
+            if(err.message === 'Unknown Member') isDevGuild = false;
+          })
+          if(isDevGuild) {
             developerServers[developerServers.length] = g.id;
+            return;
           }
         });
         /*if(premiumAPI.guildIsGold(g.id)) {
