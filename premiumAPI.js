013f48d1c3f3fddd8016a8d4941a6ef244b261b9
root <root@z3db0y.com>
2021-02-08 11:54:56 +0000

    Auto committer.

--- premiumAPI.js
+++ premiumAPI.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const dotenv = require('dotenv');
 
 let PremiumAPI = {
     guildIsBronze (guildID) {
@@ -20,6 +21,22 @@ let PremiumAPI = {
         if(guildsettings.isGold) {
             return true;
         } else return false;
+    },
+
+    getGuildType (guildID, client) {
+        let guildsettings = JSON.parse(fs.readFileSync(`${guildID}.json`));
+        let botDevelopers = JSON.parse(fs.readFileSync(process.env.CONFIG_PATH)).botDevelopers;
+        var isDevGuild;
+        botDevelopers.forEach(dev => {
+            isDevGuild = true;
+            client.guilds.resolve(guildID).members.fetch(dev) .catch(err => {
+                if(err.message === 'Unknown Member') isDevGuild = false
+            });
+            if(isDevGuild) return "Developer"
+            else if(guildsettings.isGold) return "Gold"
+            else if(guildsettings.isSilver) return "Silver"
+            else return "Bronze"
+        });
     }
 }
 
