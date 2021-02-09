01723c0ab07a40df30ee9ea9ed94802918261943
root <root@z3db0y.com>
2021-02-09 10:06:17 +0000

    Auto committer.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const updateAPI = require('./updateAPI');
 let botChannels = { "BETA":0, "STABLE":1 };
 
-const BOT_CHANNEL = botChannels.BETA;
+const BOT_CHANNEL = botChannels.STALBE;
 
 require('dotenv').config();
 const Discord = require('discord.js');
