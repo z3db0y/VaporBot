0072e238b92ec917f96e6ac84b67947157e89355
root <root@z3db0y.com>
2021-02-10 14:26:11 +0000

    Auto committer.

--- index-backup.js
+++ index-backup.js
@@ -35,7 +35,6 @@ class MusicBot {
   play(c, query) {
     this.searchYoutube(query).then(res => {
       this.add(c, res.url);
-      this.recursivePlay(c);
     });
   }
 
@@ -63,6 +62,7 @@ class MusicBot {
     if(guildsettings.musicQueue) guildsettings.musicQueue.push(url);
     else return false;
     setGuildSettings(c.channel.guild.id, guildsettings);
+    if(!c.dispatcher) this.recursivePlay(c);
     return true;
   }
 
