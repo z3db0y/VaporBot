afb1b54a1ce22936251437d2ad996f7d1715a55a
z3db0y <us3r0002@gmail.com>
2021-04-12 16:38:14 +0300

    Another bug fix.

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ let guildAPI = new class GuildAPI {
 
   initGuild(g) {
     if(fs.existsSync(`${this.guildDir}${g.id}.json`)) return console.log(`\x1b[35m[GuildAPI] \x1b[32m${g.name} \x1b[0mready!`);
-    let defaultSettings = JSON.stringify(fs.readFileSync(process.env.CONFIG_PATH).defaultSettings);
+    let defaultSettings = fs.readFileSync(process.env.CONFIG_PATH).defaultSettings;
     this.setGuildSettings(g.id, defaultSettings);
     console.log(`\x1b[35m[GuildAPI] \x1b[32m${g.name} \x1b[0mready!`);
   }
